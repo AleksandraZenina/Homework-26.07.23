3b95929a584d937f8adeea4a1ccff51a7bd4ae7f
Aleksandra Zenina <aleksandra248620@yandex.ru>
2023-07-27 21:15:23 +0500

    Task41 corrected

---
+++ Task41/Program.cs
@@ -0,0 +1,19 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//Console.Write("Введите количество элементов массива: ");
+Console.Write("Введите элементы(через пробел): ");
+int[] array = Array.ConvertAll<string, int>(Console.ReadLine().Split(), int.Parse);
+int kol(int[] array)
+{
+    int i=0;
+    int sum = 0;
+    while (i < array.Length)
+    {
+       if(array[i]>0)
+       sum = sum + 1;
+       i = i + 1;
+    }
+    return sum;
+}
+
+Console.WriteLine($"\n Количество элементов больше нуля: {kol(array)}");
+
